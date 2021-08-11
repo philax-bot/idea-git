e27a07eb73bc48c874701f0983c495cf07502d4c
Philax <1253457118@qq.com>
2021-08-11 10:51:10 +0800

    first commit

---
+++ src/main/java/com/bjpowernode/pojo/User.java
@@ -0,0 +1,35 @@
+package com.bjpowernode.pojo;
+
+/**
+ * @author Liu
+ * @create 2021-08-11 10:48
+ */
+public class User {
+    private String userid;
+    private String name;
+    private Integer age;
+    
+    public String getUserid() {
+        return userid;
+    }
+    
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public Integer getAge() {
+        return age;
+    }
+    
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
