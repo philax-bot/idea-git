a2a9e2e863e30deed389a75929a371ef9b1a1ad9
Philax <1253457118@qq.com>
2021-08-11 11:08:39 +0800

    dev commit 02

--- src/main/java/com/bjpowernode/pojo/User.java
+++ src/main/java/com/bjpowernode/pojo/User.java
@@ -13,6 +13,7 @@ public class User implements Serializable {
     private String name;
     private Integer age;
     private String address;
+    private String aa;
     
     @Override
     public String toString() {
